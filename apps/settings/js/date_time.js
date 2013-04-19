RudyLu/gaia

commit 1dd9fca32269f6cdd2e4628180cc1f18f6ef9e2a

Bug 847342 - To reset the time zone to the user selected value after the time auto update is disabled.

apps/settings/js/date_time.js
@@ -100,6 +100,18 @@ navigator.mozL10n.ready(function SettingsDateAndTime() {
 
   function setTimeAutoEnabled(enabled) {
     gTimeAutoSwitch.dataset.state = enabled ? 'auto' : 'manual';
+
+    if (enabled)
+      return;
+
+    // Reset the timezone to the preview user selected value
+    var reqUserTZ = settings.createLock().get('time.timezone.user-selected');
+    reqUserTZ.onsuccess = function dt_getUserTimezoneSuccess() {
+      var userSelTimezone = reqUserTZ.result['time.timezone.user-selected'];
+      if (userSelTimezone) {
+        settings.createLock().set({'time.timezone': userSelTimezone});
+      }
+    }
   }
 
   settings.addObserver(kTimeAutoEnabled, function(event) {

commit 346c60c2ff738f096954e664ee1c2dabf5b75947

Bug 847342 - fix the lint error introduced with commit 1dd9fca32269f6cdd2e4628180cc1f18f6ef9e2a

apps/settings/js/date_time.js
@@ -111,7 +111,7 @@ navigator.mozL10n.ready(function SettingsDateAndTime() {
       if (userSelTimezone) {
         settings.createLock().set({'time.timezone': userSelTimezone});
       }
-    }
+    };
   }
 
   settings.addObserver(kTimeAutoEnabled, function(event) {